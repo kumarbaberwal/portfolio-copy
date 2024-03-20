c1d69e42ca3ba7121627b101d52265c98a5aa797
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-03-20 23:24:06 +0530

    Scroll Transitions

--- style/static_src/tailwind.config.js
+++ style/static_src/tailwind.config.js
@@ -55,4 +55,19 @@ module.exports = {
         require('@tailwindcss/aspect-ratio'),
     ],
     darkMode:'class',
+    plugins: [
+        require('taos/plugin')
+    ],
+    safelist: [
+        '!duration-[0ms]',
+        '!delay-[0ms]',
+        'html.js :where([class*="taos:"]:not(.taos-init))'
+    ],
+    content: {
+        relative: true,
+        transform: (content) => content.replace(/taos:/g, ''),
+        files: ['../templates/**/*.html',
+        '../../templates/**/*.html',
+        '../../**/templates/**/*.html',],
+    },
 }
