4c94721f9c23742d006376c262862b8b08bc37cb
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-03-30 10:43:23 +0530

    dark mode

--- static/darkmode.js
+++ static/darkmode.js
@@ -4,4 +4,11 @@ const moon=document.querySelector("#sun");
 moon.addEventListener('click',()=>{
     body.classList.toggle('dark');
     console.log('clicked on moon.');
+    const isDarkMode = document.body.classList.contains('dark');
+    localStorage.setItem('dark',isDarkMode);
 });
+
+const darkMode = localStorage.getItem('dark');
+if (darkMode==='true'){
+    document.body.classList.add('dark');
+};
